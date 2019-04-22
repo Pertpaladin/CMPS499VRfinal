1e637f61871c91ca0ef30d84039a926cfbf2f9b3
Daniel <43864783+Samurike@users.noreply.github.com>
2019-04-22 18:59:44 -0500

    initial commit
    underwater effect, caustics, and color change on water enter

---
+++ Underwater VR Project/Assets/UnderWater.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnderWater : MonoBehaviour
+{
+    public float waterHeight;
+
+    [SerializeField] bool isUnderwater;
+
+    public Color normalColor;
+    public Color underwaterColor;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((transform.position.y < waterHeight) != isUnderwater)
+        {
+            isUnderwater = transform.position.y < waterHeight;
+            if (isUnderwater) SetUnderwater();
+            if (!isUnderwater) SetNormal();
+        }
+    }
+
+    void SetNormal()
+    {
+        RenderSettings.fog = true;
+        RenderSettings.fogColor = normalColor;
+        RenderSettings.fogDensity = 0.01f;
+
+    }
+
+    void SetUnderwater()
+    {
+        RenderSettings.fog = true;
+        RenderSettings.fogColor = underwaterColor;
+        RenderSettings.fogDensity = 0.02f;
+
+    }
+}
